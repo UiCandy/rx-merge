43cd26c9f6834b1b9c5b94a38657df682c76251b
Rishav <uicandy@live.com>
2018-10-01 16:32:45 +0200

    setting up webpack, babel, project deps

---
+++ webpack/paths.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  root: path.resolve(__dirname, '../'),
+  outputPath: path.resolve(__dirname, '../', 'build'),
+  entryPath: path.resolve(__dirname, '../', 'src/index.jsx'),
+  templatePath: path.resolve(__dirname, '../', 'src/template.html'),
+  cssFolder: 'css',
+  jsFolder: 'js',
+};
